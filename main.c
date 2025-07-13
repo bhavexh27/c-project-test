9a0924f5b2322de801fde1dda444de76a8588879
Cloud User <ec2-user@ip-172-31-33-35.us-east-2.compute.internal>
2025-07-13 17:47:31 +0000

    added files

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// Declare the functions you are using
+void biggest2();
+void factorial();
+
+int main() {
+	printf("Hello!\n");
+	biggest2();
+	factorial();
+	return 0;
+
+}
